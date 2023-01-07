470d4a27a9a638783d50afc6dd0cf69217c4f826
Lorenzo <lorenzochavezxd@gmail.com>
2023-01-06 23:07:06 -0600

    Creacion de la logica de generacion de claves (Funcion)

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ numeros = ["1","2","3","4","5","6","7","8","9","0"];
 minusculas = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","X"];
 caracteres = ["!","@","#","*","<",">","=","%","&","/"];
+var clave = [];
+var complejidad = 15;
+
+function crearclave(){
+    for(i = 1; i <= complejidad; i++){
+
+        let seleccion = Math.round(Math.random()*3);
+
+        if(seleccion == 0){
+            let item = Math.round(Math.random()*numeros.length);
+            clave.push(numeros[item]);
+        }
+        if(seleccion == 1){
+            let item = Math.round(Math.random()*minusculas.length);
+            clave.push(minusculas[item]);
+        }
+        if(seleccion == 2){
+            let item = Math.round(Math.random()*mayusculas.length);
+            clave.push(mayusculas[item]);
+        }
+        if(seleccion == 3){
+            let item = Math.round(Math.random()*caracteres.length);
+            clave.push(caracteres[item]);
+        }
+    }
+    clavefinal = clave.join("");
+    console.log(clavefinal);
+}
+crearclave();
